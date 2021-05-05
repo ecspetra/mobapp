10ad720a55492911c0487a3d190532825c462784
ecspetra <ecspetra@gmail.com>
2021-05-06 00:45:48 +0300

    fix

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".reviews-slider").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    arrows: false,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+  });
+});
